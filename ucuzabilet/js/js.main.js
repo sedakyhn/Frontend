33e967b1ba4a03eeca9c1cd0a42e3baf6d778f50
sedakyhn <seda.ckayhan@gmail.com>
2022-09-30 12:42:50 +0300

    Frontend Ödev

--- ucuzabilet/js/js.main.js
+++ ucuzabilet/js/js.main.js
@@ -1,14 +1,5 @@
-const myCarouselElement = document.querySelector('#myCarousel')
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 2000,
-  wrap: false
-})
-
-
 // Get the modal
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById("myImg");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
@@ -18,16 +9,10 @@ img.onclick = function(){
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
 }
-
-
-
 //accordion
 
 var acc = document.getElementsByClassName("accordion");
